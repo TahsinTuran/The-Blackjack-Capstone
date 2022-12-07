3af3347842c95e88b48b507a6c9cce7e3862cf93
Tahsin Turan <86520268+TahsinTuran@users.noreply.github.com>
2022-12-07 22:53:27 +0600

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,17 @@
+import random 
+
+def deal_card():
+    cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
+    card = random.choice(cards)
+    return card
+
+computer_card = []
+user_card = []
+
+for _ in range(2):
+    user_card.append(deal_card())
+    computer_card.append(deal_card()) 
+
+def calculate_cards(card):
+    return sum(card)
 
